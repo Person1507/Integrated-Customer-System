59d4e144e6b3b32f0f1aaf6f04d64e3689766927
Person1507 <51053584+Person1507@users.noreply.github.com>
2022-01-31 21:05:31 -0500

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -11,8 +11,11 @@ public class MainMenu extends JFrame {
         frame = new JFrame();
         database = new CustomerProfDB("test.txt");
 
+        // page title
         JLabel title = new JLabel("Integrated Customer System");
         title.setBounds(40, 0, 200, 60);
+
+        // all of the buttons that select the functionality the client wants to use
 
         JRadioButton opt1 = new JRadioButton("Create Profile");
         opt1.setBounds(50, 50, 200, 30);
@@ -29,6 +32,7 @@ public class MainMenu extends JFrame {
         JRadioButton opt5 = new JRadioButton("Display All Profiles");
         opt5.setBounds(50, 150, 200, 30);
 
+        // put all buttons in a button group so it looks cohesive
         ButtonGroup bg = new ButtonGroup();
         bg.add(opt1);
         bg.add(opt2);
@@ -36,6 +40,7 @@ public class MainMenu extends JFrame {
         bg.add(opt4);
         bg.add(opt5);
 
+        // add title and options to frame
         frame.add(title);
         frame.add(opt1);
         frame.add(opt2);
@@ -43,18 +48,24 @@ public class MainMenu extends JFrame {
         frame.add(opt4);
         frame.add(opt5);
 
+        // select button to go to functionality
         JButton select = new JButton("Select");
         select.setBounds(75, 200, 100, 50);
 
+        // add ActionListener to select button so that it does something
         select.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
+                // generate objects when a certain button is selected
                 if(opt1.isSelected()) new CreateProfile(database);
                 else if(opt2.isSelected()) new DeleteProfile(database);
             }
         });
 
+        // add select button to frame
         frame.add(select);
 
+        // set window size and visibility
+        // make Java runner close upon clicking X button
         frame.setSize(300, 500);
         frame.setLayout(null);
         frame.setVisible(true);
